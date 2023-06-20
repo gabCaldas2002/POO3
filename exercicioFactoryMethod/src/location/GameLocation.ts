a7253eeb18793d2beefbbf012ee5c6606e0c622e
Gabriel Caldas <127906385+gabCaldas2002@users.noreply.github.com>
2023-06-20 20:45:52 -0300

    Delete exercicioFactoryMethod directory

--- exercicioFactoryMethod/src/location/GameLocation.ts
+++
@@ -1,9 +0,0 @@
-import Game from "./type/game";
-import IItem from "./type/interface/IItem";
-import Location from "./Location";
-
-export default class GameLocation extends Location{
-    protected createItem(): IItem{
-        return new Game
-    }
-}
--- exercicioFactoryMethod/src/location/MovieLocation.ts
+++
@@ -1,9 +0,0 @@
-import IItem from "./type/interface/IItem";
-import Location from "./Location";
-import Movie from "./type/movie";
-
-export default class MovieLocation extends Location{
-    protected createItem(): IItem{
-        return new Movie
-    }
-}
