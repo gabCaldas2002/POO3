c458905f21835eb9f904800c34b1bf18296cebcb
Gabriel Caldas <127906385+gabCaldas2002@users.noreply.github.com>
2023-08-11 11:37:33 -0300

    Update home.page.ts

--- ionic/appcontatos/src/app/home/home.page.ts
+++ ionic/appcontatos/src/app/home/home.page.ts
@@ -6,10 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  nome: string = "Gabriel Caldas";
+  nome!: string;
+  telefone!: number;
+  lista_contatos: any[] = [
+    {"nome": "Gabriel Caldas", "telefone": 999757112},
+    {"nome": "Eduardo Messias", "telefone": 998254477},
+    {"nome": "Bruno Samp", "telefone": 982475631}];
 
-  constructor() {
-    console.log("Testando console");
+  constructor() {}
+
+  cadastrar(){
+    this.lista_contatos.push({"nome" : this.nome, "telefone" : this.telefone})
   }
 
 }
