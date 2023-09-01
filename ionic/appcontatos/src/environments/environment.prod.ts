5af4cabb903de518197ba39573ddaf071940789b
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2023-09-01 10:55:11 -0300

    config firebase

--- ionic/appcontatos/src/environments/environment.prod.ts
+++ ionic/appcontatos/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCLllxee8_HfHVMVakr-iIUiv8MAM3339s",
+    authDomain: "agenda-de-contatos-6fa91.firebaseapp.com",
+    projectId: "agenda-de-contatos-6fa91",
+    storageBucket: "agenda-de-contatos-6fa91.appspot.com",
+    messagingSenderId: "984231816865",
+    appId: "1:984231816865:web:78dfc6eb87a64fad2fcae0"
+  }
 };
