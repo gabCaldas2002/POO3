1b81fd273da2bdf4f54bc2d767980fb6338b31b4
Gabriel Caldas <gabriel.caldas20112002@gmail.com>
2023-09-14 20:42:28 -0300

    commit configuração inicial

---
+++ ionic/infoCarros/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDals_Qk78_YIK8VZLzzfF55-TT3SwxA0c",
+    authDomain: "infocarros-e5b8f.firebaseapp.com",
+    projectId: "infocarros-e5b8f",
+    storageBucket: "infocarros-e5b8f.appspot.com",
+    messagingSenderId: "99358319451",
+    appId: "1:99358319451:web:d9ffe4db9fc4195cc5f07e"
+  }
+}; 
