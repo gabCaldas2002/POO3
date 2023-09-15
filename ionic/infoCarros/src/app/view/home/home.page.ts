797ed9ddc6d8211171bc54fcfe5e806fbf7837bd
Gabriel Caldas <gabriel.caldas20112002@gmail.com>
2023-09-14 21:13:52 -0300

    home page

--- ionic/infoCarros/src/app/view/home/home.page.ts
+++ ionic/infoCarros/src/app/view/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Carro } from 'src/app/model/entities/Carro';
+import Portas from 'src/app/model/entities/Portas';
+import { FirebaseService } from 'src/app/model/service/firebase.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  lista_carros: Carro[] = []
 
-  constructor() {}
+  constructor(private alertController: AlertController, private router: Router, private firebase: FirebaseService) {
+    this.firebase.read()
+    .subscribe(res => {
+      this.lista_carros = res.map(carro => {
+        return {
+          id: carro.payload.doc.id,
+          ...carro.payload.doc.data() as any
+        }as Carro
+      })
+    });
+  }
 
+  irParaCadastrar(){
+    this.router.navigate(["/cadastrar"]);
+  }
+
+  editar(carro : Carro){
+    this.router.navigateByUrl("/detalhar", {state: {carro: carro}});
+  }
 }
