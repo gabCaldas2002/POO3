49e844417d4e91a0abfe45e8f948829a57dba297
Gabriel Caldas <gabriel.caldas20112002@gmail.com>
2024-01-23 22:08:12 -0300

    Classe para alerta

---
+++ ionic/infoCarros/src/app/common/alert.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { AlertController, LoadingController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor(private alertController: AlertController, private loadingController: LoadingController) { 
+  }
+
+  async presentAlert(subHeader : string, message : string) {
+    const alert = await this.alertController.create({
+      header: 'Agenda de Contatos',
+      subHeader: subHeader,
+      message: message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  simpleLoader(){
+    this.loadingController.create({
+      message: "carregando..."
+    }).then((response) => {
+      response.present();
+    })
+  }
+
+  dismissLoader(){
+    this.loadingController.dismiss().then((response) => {
+      console.log('fechou', response);
+    })
+  }
+
+}
