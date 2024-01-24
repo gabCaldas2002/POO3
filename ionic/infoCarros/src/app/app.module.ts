56f628974626f050c73206966915e34c0f12b927
Gabriel Caldas <gabriel.caldas20112002@gmail.com>
2024-01-23 22:06:48 -0300

    sign in commit

--- ionic/infoCarros/src/app/app.module.ts
+++ ionic/infoCarros/src/app/app.module.ts
@@ -7,16 +7,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import {AngularFireModule} from '@angular/fire/compat';
-import {AngularFireDatabaseModule} from '@angular/fire/compat/database';
-import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import { environment } from 'src/environments/environment.prod';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage'
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireDatabaseModule, AngularFirestoreModule, AngularFireStorageModule],
+  imports: [BrowserModule,
+     AngularFireModule.initializeApp(environment.firebaseConfig),
+     AngularFireDatabaseModule,
+     AngularFirestoreModule,
+     AngularFireStorageModule,
+     AngularFireAuthModule,
+     IonicModule.forRoot(),
+      AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- ionic/infoCarros/src/app/view/usuarios/signin/signin.page.ts
+++ ionic/infoCarros/src/app/view/usuarios/signin/signin.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/common/alert.service';
+import { AuthService } from 'src/app/model/service/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +10,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
+  formLogar: FormGroup;
 
-  constructor() { }
+
+  constructor(private alert: AlertService, private router: Router, private formBuilder: FormBuilder, private authService: AuthService) { 
+    this.formLogar = new FormGroup({
+      email: new FormControl(''),
+      senha: new FormControl('')
+    });
+  }
 
   ngOnInit() {
+    this.formLogar = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  get errorControl(){
+    return  this.formLogar.controls;
+  }
+
+  submitForm() : boolean{
+    if(!this.formLogar.valid){
+      this.alert.presentAlert('Erro', 'Erro ao preencher!')
+      return false
+    }else{
+      this.alert.simpleLoader();
+      this.logar()
+      return true;
+    }
+  }
+
+  private logar(){
+    this.authService.signIn(this.formLogar.value['email'], this.formLogar.value['senha']).then((res)=>{
+      this.alert.dismissLoader();
+      this.alert.presentAlert('Olá', 'Seja bem-vindo')
+      this.router.navigate(["/home"])
+    }).catch((error)=>{
+      this.alert.dismissLoader();
+      this.alert.presentAlert('Erro ao logar', 'Tente novamente')
+      console.log(error.message);
+    })
+    
+  }
+
+  logarComGoogle(){
+    this.authService.signInWithGoogle().then((res)=>{
+      this.alert.presentAlert('Olá', 'Seja bem-vindo')
+      this.router.navigate(["/home"])
+    }).catch((error)=>{
+      this.alert.presentAlert('Erro', 'Tente novamente')
+      console.log(error.message);
+    })
+  }
+
+  irParaSignup(){
+    this.router.navigate(["/signup"])
   }
 
 }
