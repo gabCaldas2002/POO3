c5cd5b94a65006ddad0e98993c00ebb5c4a5228f
eduardoMessKL <eduardomk408@gmail.com>
2024-03-13 23:50:29 -0300

    Componentização Carro

--- ionic/infoCarros/src/app/view/components/carro/carro.component.ts
+++ ionic/infoCarros/src/app/view/components/carro/carro.component.ts
@@ -1,9 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
-import { Carro } from 'src/app/model/entities/Carro';
-import { AuthService } from 'src/app/model/service/auth.service';
-import { FirebaseService } from 'src/app/model/service/firebase.service';
 
 @Component({
   selector: 'app-carro',
@@ -11,25 +6,9 @@ import { FirebaseService } from 'src/app/model/service/firebase.service';
   styleUrls: ['./carro.component.scss'],
 })
 export class CarroComponent  implements OnInit {
-  @Input() lista_carros: Array<Carro> = [];
-  public user: any;
+  @Input() carro: any;
 
+  constructor() {}
 
-
-  constructor(
-    private alertController: AlertController,
-    private router: Router,
-    private firebase: FirebaseService,
-    private authService: AuthService
-  ) {
-    this.user = this.authService.getUserLogged();
-    console.log(this.authService.getUserLogged());
-  }
-  ngOnInit() {
-    
-  }
-
-  editar(carro : Carro){
-    this.router.navigateByUrl("/detalhar", {state: {carro: carro}});
-  }
+  ngOnInit() {}
 }
--- ionic/infoCarros/src/app/view/components/components.module.ts
+++ ionic/infoCarros/src/app/view/components/components.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LoadingCarrosComponent } from './loading-carros/loading-carros.component';
 import { IonicModule } from '@ionic/angular';
 import { CarroComponent } from './carro/carro.component';
+import { EmptyCarrosComponent } from './empty-carros/empty-carros.component';
 
 @NgModule({
-  declarations: [LoadingCarrosComponent, CarroComponent],
+  declarations: [LoadingCarrosComponent, CarroComponent, EmptyCarrosComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [LoadingCarrosComponent, CarroComponent]
+  exports: [LoadingCarrosComponent, CarroComponent, EmptyCarrosComponent]
 })
 
 export class ComponentsModule {}
